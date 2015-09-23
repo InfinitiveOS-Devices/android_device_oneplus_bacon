8776f27ac1ec7351c21638124bffd83ba9df401c
rohan <purohit.rohan@gmail.com>
2015-09-23 08:17:35 +0200

    Add tc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,13 @@ WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
+# Kernel Toolchain
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+#KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+#TARGET_GCC_VERSION_EXP := 4.9-
+
 # inherit from the proprietary version
 ifneq ($(QCPATH),)
 -include $(QCPATH)/common/msm8974/BoardConfigVendor.mk
